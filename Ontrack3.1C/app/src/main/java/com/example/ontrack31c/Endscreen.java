0660772423af4a195f98994498f9588ece050ca2
Jayden Ellisdon <jellisdon@deakin.edu.au>
2021-03-23 15:29:45 +1100

    added activity's

---
+++ Ontrack3.1C/app/src/main/java/com/example/ontrack31c/Endscreen.java
@@ -0,0 +1,14 @@
+package com.example.ontrack31c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Endscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_endscreen);
+    }
+}
---
+++ Ontrack3.1C/app/src/main/java/com/example/ontrack31c/Question2.java
@@ -0,0 +1,14 @@
+package com.example.ontrack31c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Question2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question2);
+    }
+}
